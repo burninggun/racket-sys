579ff54a43eb178abc3e5ce9dc4b8de529b0aa41
Will Cheng <willcheng@Wills-MacBook-Pro.local>
2019-11-03 00:50:45 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, CssBaseline, FormControl, InputLabel, Input, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+	formControl: {
+		margin: theme.spacing(1)
+	}
+}))
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const classes = useStyles()
+	return (
+		<React.Fragment>
+			<CssBaseline>
+				<Container>
+
+					<FormControl className={classes.formControl} >
+						<InputLabel htmlFor="r-name" >Racket Name</InputLabel>
+						<Input id="r-name" />
+          			</FormControl>
+
+					<FormControl className={classes.formControl} >
+						<InputLabel htmlFor="s-name">String Name</InputLabel>
+						<Input id="s-name"/>
+					</FormControl>
+
+					<FormControl className={classes.formControl}>
+						<InputLabel htmlFor="s-tension">Tension (lb)</InputLabel>
+						<Input id="s-tension"/>
+					</FormControl>
+
+					<FormControl className={classes.formControl} >
+						<InputLabel htmlFor="cust-fname" >Customer First Name</InputLabel>
+						<Input id="cust-fname"/>
+					</FormControl>
+
+					<FormControl className={classes.formControl} >
+						<InputLabel htmlFor="cust-lname">Customer Last Name</InputLabel>
+						<Input id="cust-lname"/>
+					</FormControl>
+
+					<FormControl className={classes.formControl} >
+						<InputLabel htmlFor="cust-phone">Customer Phone</InputLabel>
+						<Input id="cust-phone"/>
+					</FormControl>
+
+        		</Container>
+      		</CssBaseline>
+    	</React.Fragment>
+	);
 }
 
 export default App;
