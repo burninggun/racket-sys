3f033d3b340ef8ca288ef0e97dd8f7f16851624c
burninggun <willchengsters@gmail.com>
2019-11-22 19:37:35 -0800

    connection to mysql database

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express');
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
+const mysql = require('mysql');
+
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "cheng",
+    database: "racket-system",
+    port: 8889
+})
+
+db.connect( (err) => {
+    if(err){
+        throw err
+    } else {
+        console.log('connected to database')
+    }
+} )
 
 app.listen(PORT, () => {
     console.log('Server is listening')
-})
+});
